371d23ce112271dd35fd8f495e5ec00b2dde644b
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-10-21 20:46:33 +0200

    Add Map helper

--- pkg/extensions/allocator_filter.go
+++ pkg/extensions/allocator_filter.go
@@ -1,7 +1,9 @@
 package extensions
 
 import (
+	"fmt"
 	"github.com/golang/protobuf/ptypes/any"
+	"strings"
 )
 
 type AllocatorFilterExtension struct {
@@ -13,4 +15,22 @@ func (f AllocatorFilterExtension) Any() map[string]*any.Any {
 	return map[string]*any.Any{
 		"filter": ToAny("agones.openmatch.filter", f),
 	}
+}
+
+func (f *AllocatorFilterExtension) Map() map[string]string {
+	m := map[string]string{}
+	m["labels"] = joinMapValues(f.Labels)
+	m["fields"] = joinMapValues(f.Fields)
+
+	return m
+}
+
+func joinMapValues(list map[string]string) string {
+	var values []string
+
+	for k, v := range list {
+		values = append(values, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	return strings.Join(values, ",")
 }
