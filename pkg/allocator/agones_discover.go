fa66c025fe80cf7b97217ac6e7126e3dbbda8ed3
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-10-21 21:04:31 +0200

    Test Allocate

--- pkg/allocator/agones_discover.go
+++ pkg/allocator/agones_discover.go
@@ -2,9 +2,11 @@ package allocator
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"github.com/Octops/agones-discover-openmatch/internal/runtime"
-	"math/rand"
+	"github.com/Octops/agones-discover-openmatch/pkg/extensions"
+	"github.com/golang/protobuf/ptypes/any"
+	"github.com/pkg/errors"
 	"open-match.dev/open-match/pkg/pb"
 )
 
@@ -19,18 +21,28 @@ type AgonesDiscoverAllocator struct {
 }
 
 func (c *AgonesDiscoverAllocator) Allocate(ctx context.Context, req *pb.AssignTicketsRequest) error {
-	// Extract filters from Extensions field and query Agones Discover
 	for _, group := range req.Assignments {
-		port := rand.Intn(8000-7000) + 7000
-		conn := fmt.Sprintf("%d.%d.%d.%d:%d", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256), port)
-		group.Assignment.Connection = conn
+		filter, err := ExtractFilterFromExtensions(group.Assignment.Extensions)
+		if err != nil {
+			return errors.Wrap(err, "the assignment does not have a valid filter extension")
+		}
 
-		// extension map[filter:type_url:"agones.openmatch.filter" value:"{\"labels\":{\"region\":\"us-east-1\",\"world\":\"Pandora\"},\"fields\":{\"status.state\":\"Ready\"}}" ]
-		// Stopped Here: Use Extensions["filter"] to unmarshall to extensions.AllocatorFilter and do the magic
-		//
+		resp, err := c.Client.ListGameServers(ctx, filter.Map())
+		gameservers, err := ParseGameServersResponse(resp)
+		if err != nil {
+			return errors.Wrap(err, "the response does not contain GameServers")
+		}
 
-		runtime.Logger().WithField("component", "allocator").Debugf("extension %v", group.Assignment.Extensions)
-		runtime.Logger().WithField("component", "allocator").Debugf("connection %s assigned to request", conn)
+		// TODO: Use the GameServer Player Capacity/Count field to validate if all tickets can be assigned.
+		// NiceToHave: Filter GameServers by Capacity and Count
+		if len(gameservers) > 0 {
+			group.Assignment.Connection = gameservers[0].Status.Address
+			runtime.Logger().WithField("component", "allocator").Debugf("extension %v", group.Assignment.Extensions)
+			runtime.Logger().WithField("component", "allocator").Debugf("connection %s assigned to request", group.Assignment.Connection)
+			continue
+		}
+
+		runtime.Logger().WithField("component", "allocator").Warn("request could not have a connection assigned")
 	}
 
 	return nil
@@ -38,4 +50,28 @@ func (c *AgonesDiscoverAllocator) Allocate(ctx context.Context, req *pb.AssignTi
 
 func (c *AgonesDiscoverAllocator) FindGameServer(ctx context.Context, filters map[string]string) ([]byte, error) {
 	return c.Client.ListGameServers(ctx, filters)
+}
+
+func ExtractFilterFromExtensions(extension map[string]*any.Any) (*extensions.AllocatorFilterExtension, error) {
+	if _, ok := extension["filter"]; !ok {
+		return nil, nil
+	}
+
+	filter, err := extensions.ToFilter(extension["filter"].Value)
+	if err != nil {
+		return nil, err
+	}
+
+	return filter, nil
+}
+
+func ParseGameServersResponse(resp []byte) ([]*GameServer, error) {
+	var gameservers []*GameServer
+
+	err := json.Unmarshal(resp, &gameservers)
+	if err != nil {
+		return nil, err
+	}
+
+	return gameservers, nil
 }
--- pkg/allocator/agones_discover_test.go
+++ pkg/allocator/agones_discover_test.go
@@ -2,25 +2,89 @@ package allocator
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Octops/agones-discover-openmatch/pkg/extensions"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"math/rand"
+	"open-match.dev/open-match/pkg/pb"
+	"strconv"
 	"testing"
+	"time"
 )
 
+/*
+Call Allocate and find a gameserver using the filter DeepEqual is enough
+Assign Conn to tickets
+Validate AssignTicketsRequest
+*/
 func TestAgonesDiscoverAllocator_Allocate(t *testing.T) {
-
-}
-
-func TestAgonesDiscoverAllocator_Call_FindGameServer(t *testing.T) {
-	t.Run("it should call ListGameServers", func(t *testing.T) {
+	t.Run("it should assign conn to tickets", func(t *testing.T) {
 		client := &mockAgonesDiscoverClient{}
 		discoverAllocator := &AgonesDiscoverAllocator{
 			Client: client,
 		}
 
-		client.On("ListGameServers", context.Background(), map[string]string{}).Return([]byte{})
+		filter := &extensions.AllocatorFilterExtension{
+			Labels: map[string]string{
+				"region": "us-east-1",
+			},
+			Fields: map[string]string{
+				"status.state": "Ready",
+			},
+		}
+
+		client.On("ListGameServers", context.Background(), filter.Map()).
+			Return(createGameServersWithLabels(1, filter.Labels), nil)
+
+		req := &pb.AssignTicketsRequest{
+			Assignments: []*pb.AssignmentGroup{
+				{
+					TicketIds: []string{
+						uuid.New().String(),
+						uuid.New().String(),
+						uuid.New().String(),
+					},
+					Assignment: &pb.Assignment{
+						Extensions: filter.Any(),
+					},
+				},
+			},
+		}
+
+		err := discoverAllocator.Allocate(context.Background(), req)
+		require.NoError(t, err)
+		client.AssertExpectations(t)
+
+	})
+}
+
+func TestAgonesDiscoverAllocator_Call_FindGameServer(t *testing.T) {
+	t.Run("it should call ListGameServers and not return error", func(t *testing.T) {
+		client := &mockAgonesDiscoverClient{}
+		discoverAllocator := &AgonesDiscoverAllocator{
+			Client: client,
+		}
+
+		client.On("ListGameServers", context.Background(), map[string]string{}).Return([]byte{}, nil)
 		_, err := discoverAllocator.FindGameServer(context.Background(), map[string]string{})
 		require.NoError(t, err)
+
+		client.AssertExpectations(t)
+	})
+
+	t.Run("it should call ListGameServers and return error", func(t *testing.T) {
+		client := &mockAgonesDiscoverClient{}
+		discoverAllocator := &AgonesDiscoverAllocator{
+			Client: client,
+		}
+
+		client.On("ListGameServers", context.Background(), map[string]string{}).Return([]byte{}, errors.New("error"))
+		_, err := discoverAllocator.FindGameServer(context.Background(), map[string]string{})
+		require.Error(t, err)
 
 		client.AssertExpectations(t)
 	})
@@ -32,5 +96,40 @@ type mockAgonesDiscoverClient struct {
 
 func (m *mockAgonesDiscoverClient) ListGameServers(ctx context.Context, filter map[string]string) ([]byte, error) {
 	args := m.Called(ctx, filter)
-	return args.Get(0).([]byte), nil
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func createGameServersWithLabels(count int, labels map[string]string) []byte {
+	gameservers := []*GameServer{}
+	for i := 0; i < count; i++ {
+		gs := &GameServer{
+			UID:             uuid.New().String(),
+			Name:            fmt.Sprintf("gameserver-%d", i),
+			Namespace:       "default",
+			ResourceVersion: strconv.Itoa(rand.Intn(10000)),
+			Labels:          labels,
+			Status: &GameServerStatus{
+				State:   "Ready",
+				Address: generateAddress(),
+				Players: &PlayerStatus{
+					Count:    int64(rand.Intn(100)),
+					Capacity: 100,
+					IDs:      nil,
+				},
+			},
+		}
+		gameservers = append(gameservers, gs)
+	}
+
+	b, err := json.Marshal(gameservers)
+	if err != nil {
+		return nil
+	}
+
+	return b
+}
+
+func generateAddress() string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return fmt.Sprintf("%d.%d.%d.%d:%d", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(8000-7000)+7000)
 }
