b3a78b9b37df2314ecf0db5c1e2eea6377be0881
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-11-08 16:39:41 +0100

    Add/update helper scripts

--- hack/get_certificates.sh
+++ hack/get_certificates.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Finding information about Allocator address"
 NAMESPACE=default # replace with any namespace
 #EXTERNAL_IP=$(kubectl get services agones-allocator -n agones-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 EXTERNAL_IP=$(kubectl get services agones-allocator -n agones-system -o jsonpath='{.spec.loadBalancerIP}')
@@ -10,14 +11,16 @@ TLS_FILE=${CERTS_PATH}/tls.crt
 
 # allocator-client.default secret is created only when using helm installation. Otherwise generate the client certificate and replace the following.
 # In case of MacOS replace "base64 -d" with "base64 -D"
+echo "Extracting secrets from cluster"
 kubectl get secret allocator-client.default -n "${NAMESPACE}" -ojsonpath="{.data.tls\.crt}" | base64 -d > "${CERT_FILE}"
 kubectl get secret allocator-client.default -n "${NAMESPACE}" -ojsonpath="{.data.tls\.key}" | base64 -d > "${KEY_FILE}"
 kubectl get secret allocator-tls-ca -n agones-system -ojsonpath="{.data.tls-ca\.crt}" | base64 -d > "${TLS_CA_FILE}"
 kubectl get secret allocator-tls -n agones-system -ojsonpath="{.data.tls\.crt}" | base64 -d > "${TLS_FILE}"
 
-#go run examples/allocator-client/main.go --ip ${EXTERNAL_IP} \
-#    --port 443 \
-#    --namespace ${NAMESPACE} \
-#    --key ${KEY_FILE} \
-#    --cert ${CERT_FILE} \
-#    --cacert ${TLS_CA_FILE}
+echo "Creating secrets on agones-openmatch namespace"
+kubectl -n agones-openmatch create secret tls allocator-tls-crt \
+  --cert=${CERT_FILE} \
+  --key=${KEY_FILE}
+
+kubectl -n agones-openmatch create secret generic allocator-tls-ca \
+  --from-file=tls-ca.crt=${TLS_CA_FILE}
---
+++ hack/reset_openmatch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l role=master -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l role=slave -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l role=slave -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l component=backend -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l component=frontend -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l component=query -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l component=evaluator -o jsonpath="{.items[0].metadata.name}")
+kubectl -n open-match delete pod $(kubectl -n open-match get pods -l component=synchronizer -o jsonpath="{.items[0].metadata.name}")
